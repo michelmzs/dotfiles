80f754570ddc93f875bb00b716fc2726f3af5365
Michel Z. Santello <michelmzs@gmail.com>
2022-02-19 18:00:03 -0300

    chore: remove zsh history backup; add new tg aliases

--- .michelmzs.zsh
+++ .michelmzs.zsh
@@ -26,8 +26,9 @@ alias asdf-all="cut -d ' ' -f1 .tool-versions | xargs -I{} asdf plugin-add {}"
 alias tgp="terragrunt plan"
 alias tgia="terragrunt init && terragrunt apply"
 alias tgf="terragrunt hclfmt"
-alias tgcc="find . -type d -name '.terragrunt-cache' -prune -exec rm -rf {} \;"
 alias tguk="terragrunt force-unlock"
+alias tgcc="find . -type d -name '.terragrunt-cache' -prune -exec rm -rf {} \;"
+alias tgcl="find . -type f -name '.terraform.lock.hcl' -prune -exec rm -rf {} \;"
 
 alias zreload="omz reload"
 alias zedit="vim ~/.zshrc"
@@ -65,23 +66,9 @@ function podman-cleanup() {
 	podman image prune
 }
 
-function zsh_history-backup() {
-	# TODO Refactor hard coded paths
-	todays_backups=$(find ~/tmp/backups/ -name "zsh_history_$(date +"%Y-%m-%d")*" | wc -l)
-
-	if [[ $todays_backups -eq 0 ]]; then
-		tar zcvf ~/tmp/backups/zsh_history_$(date +"%Y-%m-%d_%Hh-%Mm").gz ~/.zsh_history &>/dev/null
-		if [[ -d "/mnt/c/backup/wsl" ]]; then
-			current_backup=$(find ~/tmp/backups/ -name "zsh_history_$(date +"%Y-%m-%d")*" | sort -r | head -n 1)
-			cp "$current_backup" /mnt/c/backup/wsl
-		fi
-	fi
-}
-
 # Functions executed after terminal load
 
-# ZSH History backup
-zsh_history-backup
+# Trash recycle 
+# TODO move to systemd timer
 
-# Trash recycle
 # trash-empty 90
